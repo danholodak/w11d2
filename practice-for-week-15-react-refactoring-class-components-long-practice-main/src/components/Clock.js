4845cc0bd99a46c7583e53637c0e47c50b1e3e9e
Dan Holodak <danholodak@gmail.com>
2023-03-21 17:37:30 -0400

    up to phase5

---
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Clock.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+export function ClockToggle(props){
+
+    return (
+      <button 
+        type="button"
+        className="clock-toggle" 
+        onClick={props.toggleClock}
+      >
+        Toggle Clock
+      </button>
+    );
+}
+
+
+function Clock(props) {
+  const [time, setTime] = useState(new Date());
+  const [interval, setInt] = useState();
+  const tick = () => {
+    setTime(new Date());
+  }
+  useEffect(()=>{
+    setInt(setInterval(tick, 1000));
+
+    return (() => {
+      console.log("Clearing Clock interval!")
+      clearInterval(interval);
+    })
+  }, [])
+
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    hours = (hours < 10) ? `0${hours}` : hours;
+    minutes = (minutes < 10) ? `0${minutes}` : minutes;
+    seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+    const timezone = time
+      .toTimeString() // Form: "14:39:07 GMT-0600 (PDT)"
+      .replace(/[^A-Z]/g, "") // Strip out all but capitals
+      .slice(3); // Eliminate initial GMT
+
+    return (
+      <section className="clock-section">
+        <h1>Clock</h1>
+        <div className='clock'>
+          <p className="time">
+            <span>
+              Time:
+            </span>
+            <span>
+              {`${hours}:${minutes}:${seconds} ${timezone}`}
+            </span>
+          </p>
+          <p className="date">
+            <span>
+              Date: 
+            </span>
+            <span>
+              {time.toDateString()}
+            </span>
+          </p>
+        </div>
+      </section>
+    );
+  }
+
+export default Clock;
---
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Weather.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { toQueryString } from '../utils';
+import { useState, useEffect } from 'react';
+
+function Weather(props){
+    
+    const [weather, setWeather] = useState(null);
+    
+    
+    useEffect(()=>{
+      const  pollWeather = async (location) => {
+        let url = 'http://api.openweathermap.org/data/2.5/weather?';
+        const apiKey = process.env.REACT_APP_WEATHER_API;
+
+        const params = {
+          lat: location.coords.latitude,
+          lon: location.coords.longitude,
+          appid: apiKey
+        };
+        
+        url += toQueryString(params);
+        
+        const res = await fetch(url);
+        if (res.ok) {
+          const data = await res.json();
+          setWeather(data);
+        }
+        else {
+          alert ("Check Weather API key!")
+        }
+      }
+      navigator.geolocation?.getCurrentPosition(
+        pollWeather,
+        (err) => console.log(err),
+        { timeout: 10000 }
+      );
+    }, []);
+    
+    let content = <div className='loading'>loading weather...</div>;
+    
+    if (weather) {
+      const temp = (weather.main.temp - 273.15) * 1.8 + 32;
+      content = (
+        <div>
+          <p>{weather.name}</p>
+          <p>{temp.toFixed(1)} degrees</p>
+        </div>
+      );
+    }
+    else {
+      content = (
+        <div>
+          Weather is currently unavailable. (Are Location Services enabled?) 
+        </div>
+      )
+    }
+
+    return (
+      <section className="weather-section">
+        <h1>Weather</h1>
+        <div className='weather'>
+          {content}
+        </div>
+      </section>
+    );
+  }
+
+
+export default Weather;
